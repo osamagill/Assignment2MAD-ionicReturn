7f86e378109c0eb7230020bfa494a0a20361efac
osamagill <osama.bsse3321@iiu.edu.pk>
2019-10-28 06:34:35 -0700

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  { path: 'single-item', loadChildren: './single-item/single-item.module#SingleItemPageModule' },
+  { path: 'add-item', loadChildren: './add-item/add-item.module#AddItemPageModule' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/single-item/single-item.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-item',
+  templateUrl: './single-item.page.html',
+  styleUrls: ['./single-item.page.scss'],
+})
+export class SingleItemPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
